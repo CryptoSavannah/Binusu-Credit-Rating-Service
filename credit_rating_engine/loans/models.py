2c106742f5f5bacfbd4a21b241712eebe93291c5
Kapy <kapsonkatongole@gmail.com>
2019-11-20 13:24:26 +0300

    Add new credit score tables

--- credit_rating_engine/loans/models.py
+++ credit_rating_engine/loans/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import User
 
 class Loans(models.Model):
     borrower_address        = models.CharField(max_length=108)
@@ -23,22 +24,25 @@ class LoanPayments(models.Model):
     repayment_penalty       = models.BooleanField(default=False)
     penalty_amount          = models.DecimalField(max_digits=2, decimal_places=2, blank=True, null=True)
 
-class CreditScores(models.Model):
-    score_owner_address             = models.CharField(max_length=108)
-    credit_refferal                 = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    network_contribution            = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    new_credit                      = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)   
-    loan_frequency                  = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    initial_repayment_installment   = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    average_number_of_installments  = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    loan_completion                 = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    late_payment_penalties          = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    final_score                     = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    date_computed                   = models.DateTimeField(auto_now_add=True)
-
 class LendingRank(models.Model):
     rank_owner_address              = models.CharField(max_length=108)
     number_of_loans_monthly         = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     amount_lent_monthly             = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     final_score                     = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     date_computed                   = models.DateTimeField(auto_now_add=True)
+
+class ScoreMetric(models.Model):
+    metric_title                    = models.CharField(max_length=255)
+    metric_percentage_contribution  = models.DecimalField(max_digits=5, decimal_places=2)
+    metric_description              = models.TextField()
+    metric_classification           = models.CharField(max_length=15)
+    date_added                      = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{}".format(self.metric_title)
+
+class CreditScoreCalculation(models.Model):
+    related_address       = models.ForeignKey(User, on_delete=models.CASCADE, related_name="score_owner")
+    related_metric        = models.ForeignKey(ScoreMetric, on_delete=models.CASCADE, related_name="metric_id")          
+    metric_score          = models.IntegerField()  
+    date_computed         = models.DateTimeField(auto_now_add=True)  
